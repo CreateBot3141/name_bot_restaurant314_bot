7dbd2baa816ef8eae05d5e78a385a395ca8ebef6
3dot14 <88181088+CreateBot3141@users.noreply.github.com>
2022-06-08 16:05:37 +0500

    Add files via upload

---
+++ name_bot_restaurant314_bot.py
@@ -0,0 +1,6 @@
+def start_prog (user_id,namebot,message_in,status,message_id,name_file_picture,telefon_nome):
+    import iz_func
+    import iz_game
+    import iz_main
+    import time
+    import iz_telegram
